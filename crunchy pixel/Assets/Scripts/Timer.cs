2e6968a65f64def78cfa8c3ff4d3720b10fdd495
Ryan Comester <ryancomester@gmail.com>
2021-11-21 19:39:05 +0530

    game over on timeout

--- crunchy pixel/Assets/Scripts/Timer.cs
+++ crunchy pixel/Assets/Scripts/Timer.cs
@@ -7,7 +7,16 @@ public class Timer : MonoBehaviour
 {
     public float timeValue = 180;
     public Text timeText;
-    
+    public GameObject gameOverPanel;
+
+    public static bool isGameOver;
+
+    private void Start()
+    {
+        isGameOver = false;
+        Time.timeScale = 1;
+    }
+
     void Update()
     {
         if (timeValue > 0)
@@ -16,10 +25,18 @@ public class Timer : MonoBehaviour
         }
         else
         {
+            //timeout
             timeValue = 0;
+            isGameOver = true;
         }
 
         DisplayTime(timeValue);
+
+        if (isGameOver)
+        {
+            gameOverPanel.SetActive(true);
+            StartCoroutine(GameOver());
+        }
     }
 
     void DisplayTime(float timeToDisplay)
@@ -34,6 +51,14 @@ public class Timer : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    IEnumerator GameOver()
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("gameover");
+        Time.timeScale = 0;
+        yield return null;
     }
 
 }
